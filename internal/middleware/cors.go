4e2bffdcc5a4a4f91adb2102d7d554a1bdf954af
ftorn1k <154966484+ftorn1k@users.noreply.github.com>
2026-01-19 02:17:49 +0300

    fix: cors

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -6,7 +6,8 @@ func WithCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Max-Age", "600")
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusNoContent)
 			return
