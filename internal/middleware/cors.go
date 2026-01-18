497a9f59d7be77b3b9a4e42e97c513e34a93d262
Son1kXDev <son1kx@vk.com>
2026-01-19 01:54:22 +0300

    fix: CORS "slash"

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func WithCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "https://bookpulse.enjine.ru/")
+		w.Header().Set("Access-Control-Allow-Origin", "https://bookpulse.enjine.ru")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 		if r.Method == http.MethodOptions {
