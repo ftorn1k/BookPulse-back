e29f5a2be07a7a05373313b7c8adc5755fbd62eb
Son1kXDev <son1kx@vk.com>
2026-01-19 01:51:10 +0300

    update: cors

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func WithCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200")
+		w.Header().Set("Access-Control-Allow-Origin", ["http://localhost:4200", "https://bookpulse.enjine.ru/"])
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 		if r.Method == http.MethodOptions {
